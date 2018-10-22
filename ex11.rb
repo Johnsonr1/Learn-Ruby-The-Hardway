92d42778cc0e1776bffb902fc2225070df07b96c
Johnsonr1 <r.t.johnson@btopenworld.com>
2018-10-22 20:02:16 +0200

    First Commit

---
+++ ex11.rb
@@ -0,0 +1,17 @@
+#puts "How old are you? "
+#age = gets
+#print "How tall are you? "
+#height = gets.chomp
+#print "How much do you weigh? "
+#weight = gets.chomp
+
+#puts "So, you're #{age} old, #{height} tall and #{weight} heavy."
+
+print "How old are you? "
+age = gets.chomp
+print "How tall are you? "
+height = gets.chomp
+print "How much do you weigh? "
+weight = gets.chomp
+
+puts "So, you're #{age} old, #{height} tall and #{weight} heavy."
